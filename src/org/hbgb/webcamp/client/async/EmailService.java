dfe76e74022b0810fced19d27d35a9db6199829e
Michael Grouse <michael.grouse@gmail.com>
2018-04-12 18:20:20 -0700

    one more
    cvXCv

---
+++ src/org/hbgb/webcamp/client/async/EmailService.java
@@ -0,0 +1,20 @@
+/*
+ * Decompiled with CFR 0_115.
+ * 
+ * Could not load the following classes:
+ * com.google.gwt.user.client.rpc.RemoteService
+ * com.google.gwt.user.client.rpc.RemoteServiceRelativePath
+ */
+package org.hbgb.webcamp.client.async;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath(value = "emailService")
+public interface EmailService extends RemoteService
+{
+	public String sendApplicationRecievedEmail(String var1) throws Exception;
+
+	public String sendEmailToCircle(String circle, String subject, String message, String replyName,
+			String replyAddress);
+}
